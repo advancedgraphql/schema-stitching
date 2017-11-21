843cf3a3611a9c43a0587fb1c3ce734d50958820
Nikolas Burk <nikolas.burk@gmail.com>
2017-11-21 11:24:53 +0100

    add example: merge-schemas-1

---
+++ merge-schemas-1/src/index.js
@@ -0,0 +1,49 @@
+const { makeExecutableSchema, mergeSchemas } = require('graphql-tools')
+const { GraphQLSchema, graphql } = require('graphql')
+const { schemaA, schemaB } = require('./schemas')
+
+/*
+Schema A: 
+
+  type Query {
+    hello: String
+  }
+
+Schema B:
+
+  type Query {
+    goodbye: String
+  }
+  type Mutation {
+    launchMissiles: Boolean
+  }
+
+Merged schema: 
+
+  type Query {
+    hello: String   # originates from schema A
+    goodbye: String # originates from schema B
+  }
+
+  type Mutation {
+    launchMissiles: Boolean # originates from schema B
+  }
+
+*/
+
+const mergedSchema = mergeSchemas({ schemas: [schemaA, schemaB] })
+
+const helloQuery = `{hello}`
+graphql(mergedSchema, helloQuery).then(result => {
+  console.log(`Query:\n  ${helloQuery}\nResult:\n  ${JSON.stringify(result)}`)
+})
+
+const goodbyeQuery = `{goodbye}`
+graphql(mergedSchema, goodbyeQuery).then(result => {
+  console.log(`Query:\n  ${goodbyeQuery}\nResult:\n  ${JSON.stringify(result)}`)
+})
+
+const launchMissilesMutation = `mutation{launchMissiles}`
+graphql(mergedSchema, launchMissilesMutation).then(result => {
+  console.log(`Mutation:\n  ${launchMissilesMutation}\nResult:\n  ${JSON.stringify(result)}`)
+})
---
+++ merge-schemas-1/src/schemas.js
@@ -0,0 +1,41 @@
+const { makeExecutableSchema } = require('graphql-tools')
+
+const makeSchemaA = () => {
+  const typeDefs = `
+  type Query {
+    hello: String
+  }`
+
+  const resolvers = {
+    Query: {
+      hello: () => 'Hello'
+    }
+  }
+  return makeExecutableSchema({ typeDefs, resolvers })
+}
+
+const makeSchemaB = () => {
+  const typeDefs = `
+  type Query {
+    goodbye: String
+  }
+  
+  type Mutation {
+    launchMissiles: Boolean
+  }`
+
+  const resolvers = {
+    Query: {
+      goodbye: () => 'Goodbye'
+    },
+    Mutation: {
+      launchMissiles: () => Math.random() >= 0.5
+    }
+  }
+  return makeExecutableSchema({ typeDefs, resolvers })
+}
+
+module.exports = {
+  schemaA: makeSchemaA(), 
+  schemaB: makeSchemaB()
+}
